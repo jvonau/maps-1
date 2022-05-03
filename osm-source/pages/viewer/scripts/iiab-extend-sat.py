b2438fca8c3717670d7f222bc1981deed25bc11e
Jerry Vonau <jvonau3@gmail.com>
2022-05-03 04:12:59 -0500

    whitespace and indents part 1b - break for night

--- osm-source/pages/viewer/scripts/iiab-extend-sat.py
+++ osm-source/pages/viewer/scripts/iiab-extend-sat.py
@@ -57,75 +57,75 @@ from math import *
 
 class Tools(object):
     def numTiles(self,z):
-         return(pow(2,z))
+        return(pow(2,z))
 
     def sec(self,x):
-         return(1/cos(x))
+        return(1/cos(x))
 
     def latlon2relativeXY(self,lat,lon):
-         x = (lon + 180) / 360
-         y = (1 - log(tan(radians(lat)) + self.sec(radians(lat))) / pi) / 2
-         return(x,y)
+        x = (lon + 180) / 360
+        y = (1 - log(tan(radians(lat)) + self.sec(radians(lat))) / pi) / 2
+        return(x,y)
 
     def latlon2xy(self,lat,lon,z):
-         n = self.numTiles(z)
-         x,y = self.latlon2relativeXY(lat,lon)
-         return(n*x, n*y)
+        n = self.numTiles(z)
+        x,y = self.latlon2relativeXY(lat,lon)
+        return(n*x, n*y)
 
     def tileXY(self,lat, lon, z):
-         x,y = self.latlon2xy(lat,lon,z)
-         return(int(x),int(y))
+        x,y = self.latlon2xy(lat,lon,z)
+        return(int(x),int(y))
 
     def xy2latlon(self,x,y,z):
-         n = self.numTiles(z)
-         relY = y / n
-         lat = self.mercatorToLat(pi * (1 - 2 * relY))
-         lon = -180.0 + 360.0 * x / n
-         return(lat,lon)
+        n = self.numTiles(z)
+        relY = y / n
+        lat = self.mercatorToLat(pi * (1 - 2 * relY))
+        lon = -180.0 + 360.0 * x / n
+        return(lat,lon)
 
     def latEdges(self,y,z):
-         n = numTiles(z)
-         unit = 1 / n
-         relY1 = y * unit
-         relY2 = relY1 + unit
-         lat1 = self.mercatorToLat(pi * (1 - 2 * relY1))
-         lat2 = self.mercatorToLat(pi * (1 - 2 * relY2))
-         return(lat1,lat2)
+        n = numTiles(z)
+        unit = 1 / n
+        relY1 = y * unit
+        relY2 = relY1 + unit
+        lat1 = self.mercatorToLat(pi * (1 - 2 * relY1))
+        lat2 = self.mercatorToLat(pi * (1 - 2 * relY2))
+        return(lat1,lat2)
 
     def lonEdges(self,x,z):
-         n = numTiles(z)
-         unit = 360 / n
-         lon1 = -180 + x * unit
-         lon2 = lon1 + unit
-         return(lon1,lon2)
+        n = numTiles(z)
+        unit = 360 / n
+        lon1 = -180 + x * unit
+        lon2 = lon1 + unit
+        return(lon1,lon2)
 
     def tileEdges(self,x,y,z):
-         lat1,lat2 = latEdges(y,z)
-         lon1,lon2 = lonEdges(x,z)
-         return((lat2, lon1, lat1, lon2)) # S,W,N,E
+        lat1,lat2 = latEdges(y,z)
+        lon1,lon2 = lonEdges(x,z)
+        return((lat2, lon1, lat1, lon2)) # S,W,N,E
 
     def mercatorToLat(self,mercatorY):
-         return(degrees(atan(sinh(mercatorY))))
+        return(degrees(atan(sinh(mercatorY))))
 
     def tileSizePixels(self):
-         return(256)
+        return(256)
 
     def tileLayerExt(self,layer):
-         if(layer in ('oam')):
-             return('jpg')
-             return('png')
+        if(layer in ('oam')):
+            return('jpg')
+            return('png')
 
     def tileLayerBase(self,layer):
-         layers = { \
-             "tah": "http://cassini.toolserver.org:8080/http://a.tile.openstreetmap.org/+http://toolserver.org/~cmarqu/hill/",
-             #"tah": "http://tah.openstreetmap.org/Tiles/tile/",
-             "oam": "http://oam1.hypercube.telascience.org/tiles/1.0.0/openaerialmap-900913/",
-             "mapnik": "http://tile.openstreetmap.org/mapnik/"
-           }
-         return(layers[layer])
+        layers = { \
+            "tah": "http://cassini.toolserver.org:8080/http://a.tile.openstreetmap.org/+http://toolserver.org/~cmarqu/hill/",
+            #"tah": "http://tah.openstreetmap.org/Tiles/tile/",
+            "oam": "http://oam1.hypercube.telascience.org/tiles/1.0.0/openaerialmap-900913/",
+            "mapnik": "http://tile.openstreetmap.org/mapnik/"
+        }
+        return(layers[layer])
 
     def tileURL(self,x,y,z,layer):
-         return "%s%d/%d/%d.%s" % (self.tileLayerBase(layer),z,x,y,self.tileLayerExt(layer))
+        return "%s%d/%d/%d.%s" % (self.tileLayerBase(layer),z,x,y,self.tileLayerExt(layer))
 
 class MBTiles():
     def __init__(self, filename):
